947408ea187e74964ac5453fa66d873d43f40bcd
hanaset <b183523@gmail.com>
2016-11-08 20:35:11 +0900

    barcode_scan

--- book_admin_app/app/src/main/java/com/example/jeongbin/test/LoginActivity.java
+++ book_admin_app/app/src/main/java/com/example/jeongbin/test/LoginActivity.java
@@ -4,11 +4,19 @@ package com.example.jeongbin.test;
  * Created by JeongBin on 2016-11-02.
  */
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.net.MalformedURLException;
 
@@ -54,8 +62,39 @@ public class LoginActivity extends AppCompatActivity {
                 return_num.setText(book_num[2]+" 개가 있습니다.");
             }
 
+            Button book_add_btn = (Button)findViewById(R.id.login_book_add_btn);
+            final Activity activity = this;
+            book_add_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    IntentIntegrator integrator = new IntentIntegrator(activity);
+                    integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                    integrator.setPrompt("Scan");
+                    integrator.setCameraId(0);
+                    integrator.setBeepEnabled(false);
+                    integrator.setBarcodeImageEnabled(false);
+                    integrator.initiateScan();
+                }
+            });
+
         }catch (MalformedURLException e){
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            if(result.getContents() == null){
+                Log.d("LoginActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            }else{
+                Log.d("LoginActivity", "Scanned");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 }
